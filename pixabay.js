f190261c9fb810646eeb98db8bb421cee583bfb6
Elias Perreira Miguel <eliasperreiramiguel@gmail.com>
2024-06-16 18:45:26 -0300

    atualizando as variaveis

--- pixabay.js
+++ pixabay.js
@@ -1,3 +1,106 @@
+
+
+const buscarImagemAPI = async (texto)=>{
+  const key = '12180485-63058e50c2824559402449f76';
+  const url = `https://pixabay.com/api/?key=${key}&q=${texto}`;
+  const response = await fetch(url)
+  return await response.json()
+}
+
+const carregarImagem = async (texto, page=1)=>{
+const container = document.querySelector('.container-gallery')
+const {hits, totalHits} = await buscarImagemAPI(`${texto}&page=${page}`)
+const cardes = hits.map(criarCardes)
+container.replaceChildren(...cardes)
+
+
+const totalPaginas = Math.ceil(totalHits / 20)
+
+  document.querySelector('#search-input').value = texto;
+  document.querySelector('#page-total').textContent = `/ ${totalPaginas}`;
+  document.querySelector('#page').value = page;
+  
+}
+
+const criarCardes =({webformatURL, pageURL, likes, comments, tags})=>{
+  const card =document.createElement('div')
+  card.setAttribute('class', 'card-container')
+  card.innerHTML= `
+  <a href=${pageURL} class='card-image'> 
+     <img src='${webformatURL}'>
+  </a> 
+  <div class="card-info"> 
+    <div class="card-tags">
+      ${tags.split(',').map(criarTag).join('')}
+    </div>
+    <div class="card-action">  
+        <div class="card-like">
+            <i class="far fa-thumbs-up"></i>
+            <span>${likes}</span>
+        </div>
+        <div class="card-comment">
+            <i class="far fa-comment"></i>
+            <span>${comments}</span>
+        </div>
+        <div class="card-save">
+            <i class="far fa-bookmark"></i>
+        </div>
+    </div>
+  </div>
+  `
+ return card
+}
+const criarTag = (tag)=>`<a href='#'<a href="#" onClick="carregarImagem('${tag}')">
+${tag}
+</a>
+  `
+
+
+const pesquisarImagem =({key, target})=> {
+  if(key === "Enter"){
+    carregarImagem(target.value) 
+  }
+}
+
+
+const ligarPagina = ({key, target})=>{
+ const texto = document.getElementById('page')
+ if(key === 'Enter'){
+  carregarImagem(texto, target.value )
+ }
+
+}
+
+const proximaPagina = ()=>{
+  let pagina  = Number(document.querySelector('#page').value)
+  console.log(pagina)
+  const totalPaginas = Number(document.querySelector('#page-total').textContent.replace('/', ''));
+  const texto = document.querySelector('#search-input').value;
+  if (pagina< totalPaginas) {
+      pagina++;
+      carregarImagem(texto, pagina);
+  }
+
+}
+
+const paginaAnterior = ()=>{
+  let pagina  = Number(document.querySelector('#page').value);
+  
+  const texto = document.querySelector('#search-input').value;
+  if (pagina > 1) {
+      pagina--;
+      carregarImagem(texto, pagina);
+  }
+}
+document.getElementById('page').addEventListener('keypress', ligarPagina)
+document.getElementById('search-input').addEventListener('keypress', pesquisarImagem)
+document.querySelector('#page-next').addEventListener('click', proximaPagina);
+document.querySelector('#page-previous').addEventListener('click', paginaAnterior)
+
+
+
+
+
 const searchImages = async (text) => {
   const key = '12180485-63058e50c2824559402449f76';
   const url = `https://pixabay.com/api/?key=${key}&q=${text}`;
@@ -89,3 +192,4 @@ document.querySelector('#search-input').addEventListener('keypress', handleKeypr
 document.querySelector('#page').addEventListener('keypress', handlePage);
 document.querySelector('#page-next').addEventListener('click', handleNext);
 document.querySelector('#page-previous').addEventListener('click', handlePrevious);
+*/
